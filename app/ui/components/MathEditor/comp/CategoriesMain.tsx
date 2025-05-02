22d1fee3b7f081d941c79786c941429c4de727fb
Raghavendra K J <raghavendra.kj@bluecrimson.in>
2025-05-02 21:21:54 +0530

    refactor: remove MathEditor components and related styles
    - Deleted KeyboardFooter, KeyboardView, MathEditor, MathEditorContext, MathInputField, and associated components.
    - Removed models related to categories, key groups, and sections.
    - Cleaned up styles related to the MathEditor.

--- app/ui/components/MathEditor/comp/CategoriesMain.tsx
+++
@@ -1,9 +0,0 @@
-import { observer } from "mobx-react-lite";
-import styles from "./../style.module.css";
-import { useMathEditorStore } from "../MathEditorContext";
-
-
-export const CategoriesMain = observer(() => {
-    const store = useMathEditorStore();
-    return (<div className={styles.categoryMain}>{store.category.child.renderer(store.category)}</div>);
-});
